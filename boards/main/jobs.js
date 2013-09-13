12672bd011d7c0087df4116bded9af4ac40b02f4
Ramanan Sivaranjan <ramanan@funkaoshi.com>
2013-09-13 12:07:35 -0400

    Initial code to generate a Jenkins list.

--- boards/main/jobs.js
+++ boards/main/jobs.js
@@ -1,9 +1,15 @@
 //
 // Jobs (Backend)
 //
+//
+
+
+var request = require('request'),
+    _       = require('underscore');
+
 module.exports = {
     'weather': {
-        interval: 5,
+        interval: 60,
         fetch: function(job) {
             job.continue({
                 temperature: 'HOT AS FUCK',
@@ -20,6 +26,7 @@ module.exports = {
         }
     },
     'transit': {
+        interval: 60,
         fetch: function(job) {
             job.continue({
                 delays: 'everyday',
@@ -28,6 +35,7 @@ module.exports = {
         }
     },
     'iteration': {
+        interval: 60,
         fetch: function(job) {
             job.continue({
                 milestone: '2.24',
@@ -35,4 +43,27 @@ module.exports = {
             });
         }
     },
-}
+    'jenkins': {
+        interval: 5, // for testing
+        fetch: function(job) {
+            var colormap = {
+                'red': 'failure',
+                'blue': 'success',
+                'white': 'running'
+            }
+            var jobs = [];
+            request.get({
+                url: "http://192.168.1.16:8080/api/json",
+                json: true
+            }, function (error, response, body) {
+                jobs = _(body.jobs).map(function (job) {
+                   return {'name': job.name, 'status': colormap[job.color]};
+                });
+                job.continue({
+                    jobs: jobs
+                });
+            });
+
+        }
+    }
+};
