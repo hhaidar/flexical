d2ac8d06b5632c0f7024221b5b96f8bb10b29338
Houssam Haidar <houssam@sdelements.com>
2013-09-24 01:11:35 -0500

    Add skycons

--- widgets/weather/job.js
+++ widgets/weather/job.js
@@ -10,10 +10,44 @@ var weatherJob = function(job) {
             // YOLO
             return;
         }
+        // See http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes#Icon-list
+        /*****
+            CLEAR_DAY
+            CLEAR_NIGHT
+            PARTLY_CLOUDY_DAY
+            PARTLY_CLOUDY_NIGHT
+            CLOUDY
+            RAIN
+            SLEET
+            SNOW
+            WIND
+            FOG
+        *****/
+        var iconMap = {
+            '01d': 'CLEAR_DAY',
+            '01n': 'CLEAR_NIGHT',
+            '02d': 'PARTLY_CLOUDY_DAY',
+            '02n': 'PARTLY_CLOUDY_NIGHT',
+            '03d': 'CLOUDY',
+            '03n': 'CLOUDY',
+            '04d': 'CLOUDY',
+            '04n': 'CLOUDY',
+            '09d': 'RAIN',
+            '09n': 'RAIN',
+            '10d': 'RAIN',
+            '10n': 'RAIN',
+            '11d': 'RAIN',
+            '11n': 'RAIN',
+            '13d': 'SNOW',
+            '13n': 'SNOW',
+            '50d': 'FOG',
+            '50n': 'FOG'
+        }
         job.continue({
             location: data.name,
             title: data.weather[0].main,
             description: data.weather[0].description,
+            icon: iconMap[data.weather[0].icon] || iconMap['CLEAR_DAY'],
             temp: Math.round((data.main.temp - 273.15) * 10) / 10
         });
     });
--- widgets/weather/view.js
+++ widgets/weather/view.js
@@ -1,6 +1,14 @@
 (function() {
     Flexical.views.weather = Flexical.Widget.extend({
+        init: function() {
+            this.skycons = new Skycons({
+                'color': '#fff'
+            })
+            this.skycons.add(this.$('[flxl-id=skycon]').get(0), Skycons.CLEAR_DAY);
+            this.skycons.play();
+        },
         update: function(weather) {
+            this.skycons.set(this.$('[flxl-id=skycon]').get(0), Skycons[weather.icon]);
             this.$('[flxl-id=location]').text(weather.location);
             this.$('[flxl-id=temp]').text(weather.temp);
             this.$('[flxl-id=title]').text(weather.title);
