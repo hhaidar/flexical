add19c40e4075dd19a92e2c3397c3d7b24253c14
Houssam Haidar <houssam@sdelements.com>
2013-09-25 00:53:34 -0500

    Add loading screen

--- media/js/flexical.js
+++ media/js/flexical.js
@@ -5,15 +5,18 @@
     // Board
     //
     Flexical.Board = Backbone.View.extend({
+        el: 'body',
         initialize: function() {
             this.widgets = {};
-            _.each(this.options.widgets, function(widget, id) {
-                this.widgets[id] = new (Flexical.views[widget.type] || Flexical.Widget)(widget);
+            _.each(this.options.widgets, function(widget) {
+                this.addWidget(new (Flexical.views[widget.type] || Flexical.Widget)(widget));
+                this.widgets[widget.id].once('loaded', this.doneLoading, this);
             }, this);
             this.listen();
+            this.render();
         },
         render: function() {
-            // HUEHUEHUE
+            this.$('.flxl-widgets').addClass('hide');
         },
         listen: function() {
             var that = this;
@@ -26,6 +29,22 @@
         },
         addWidget: function(widget) {
             this.widgets[widget.id] = widget;
+        },
+        doneLoading: function() {
+            var notLoaded = _.filter(this.widgets, function(widget) {
+                return widget.loaded != true;
+            }, this);
+            var loaded = _.size(this.widgets) - _.size(notLoaded);
+            var status = loaded + '/' + _.size(this.widgets) + ' Widgets';
+            this.$('.flxl-loading-status').text(status);
+            if (_.size(notLoaded) <= 0) {
+                setTimeout(function() {
+                    this.$('.flxl-loading').addClass('hide');
+                    setTimeout(function() {
+                        this.$('.flxl-widgets').removeClass('hide');
+                    }, 200);
+                }, 1 * 1500);
+            }
         }
     });
     //
@@ -38,6 +57,7 @@
             this.init = this.options.init || this.init;
             this.update = this.options.update || this.update;
             this.on('job.update', this.update, this);
+            this.once('job.update', this.doneLoading, this);
             this.init();
         },
         init: function() {
@@ -46,6 +66,10 @@
         update: function(data) {
             console.log(data);
             // Placeholder ATM
+        },
+        doneLoading: function() {
+            this.loaded = true;
+            this.trigger('loaded');
         }
     });
     window.Flexical = Flexical;
--- widgets/clock/view.js
+++ widgets/clock/view.js
@@ -2,11 +2,14 @@
     Flexical.views.clock = Flexical.Widget.extend({
         init: function() {
             var that = this;
-            setInterval(function() {
+            var makeTime = function() {
                 var time = moment().format('hh:mm')
                   + '<small>' + moment().format('A') + '<small>';             
                 that.$el.find('time').html(time);
-            }, 500);
+            }
+            makeTime();
+            setInterval(makeTime, 500);
+            this.doneLoading();
         }
     });
 })();
