4cc2dbf74b9dbc1e5ba1ddedbd3d3a159df0d2f0
Houssam Haidar <houssam@sdelements.com>
2013-09-30 22:31:21 -0500

    Style weather widget

--- widgets/weather/view.js
+++ widgets/weather/view.js
@@ -4,15 +4,14 @@
             this.skycons = new Skycons({
                 'color': '#fff'
             })
-            this.skycons.add(this.$('[flxl-id=skycon]').get(0), Skycons.CLEAR_DAY);
+            this.skycons.add(this.$('[data-id=skycon]').get(0), Skycons.CLEAR_DAY);
             this.skycons.play();
         },
         update: function(weather) {
-            this.skycons.set(this.$('[flxl-id=skycon]').get(0), Skycons[weather.icon]);
-            this.$('[flxl-id=location]').text(weather.location);
-            this.$('[flxl-id=temp]').text(weather.temp);
-            this.$('[flxl-id=title]').text(weather.title);
-            this.$('[flxl-id=desc]').text(weather.description);
+            this.skycons.set(this.$('[data-id=skycon]').get(0), Skycons[weather.icon]);
+            this.$('[data-id=temp]').text(weather.temp);
+            this.$('[data-id=title]').text(weather.title);
+            this.$('[data-id=date]').text(moment().format('dddd Do, MMMM'));
         }
     });
 })();
